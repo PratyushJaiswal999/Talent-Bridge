75f0824b4b0def1d0d569b345de9372bee2044a0
Pratyush Jaiswal <pratyushjaiswal999@gmail.com>
2025-11-22 14:21:52 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {ENV} from "./lib/env.js";
+
+const app = express();
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+app.get("/", (req,res) => {
+    res.status(200).json({msg:"success from backend 123"})
+})
+
+app.get("/pratyush", (req,res) => {
+    res.status(200).json({msg:"Pratyush is UP and WORKING!!!!!"})
+})
+
+app.listen(ENV.PORT, () => console.log("Server is running on port:",ENV.PORT));
+
+
